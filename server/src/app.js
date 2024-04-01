88910b0564db4b272492599beb9b7e925cb0c72f
sachingour <gour.sachin.sg@gmail.com>
2024-04-02 00:02:29 +0530

    error handle in backend

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const cockieParser = require("cookie-parser");
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" })); //Json Handle with Body
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //Json Handle with URL
+app.use(express.static("public")); //use for static items
+app.use(cockieParser()); //Server to user's Browser cookies access for tokens
 
 module.exports = app;
--- server/src/db/index.js
+++ server/src/db/index.js
@@ -4,7 +4,7 @@ const { DB_Name } = require("../constant/constant.js");
 const connectDB = async () => {
   try {
     const connectionInstance = mongoose.connect(`${process.env.MONGODB_URI}/${DB_Name}`);
-    console.log(`\n MongoDB connected !! DB HOST:${connectionInstance?.connection?.host}`);
+    console.log(`MongoDB connected !! DB HOST:${connectionInstance?.connection?.host}`);
   } catch (error) {
     console.log("MONGODB Connect Failed", error);
     process.exit(1);
