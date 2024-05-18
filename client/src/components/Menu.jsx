9e80d0e03c8260a1136985081b239e5a14cdbc86
sachingour <gour.sachin.sg@gmail.com>
2024-05-18 22:53:46 +0530

    menu items

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,23 +1,56 @@
-import React from 'react'
-import {menuData} from "../constantFiles/menuContent"
+import React, { useState } from "react";
+import { menuData } from "../constantFiles/menuContent";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import f1 from "../assets/images/f1.png";
 
 const Menu = () => {
-  console.log(menuData);
+  const [foodItems, setFoodItems] = useState(menuData);
   return (
-    <section className='menuSection'>
-        <div className='menuWrapper'>
-            <div className='secHeading'>
-                <h2>Our Menu</h2>
-            </div>
-            <div className='contentContainer'>
-                <div className=''>
-
-                </div>
-            </div>
+    <section className="menuSection">
+      <div className="menuWrapper">
+        <div className="secHeading">
+          <h2>Our Menu</h2>
         </div>
-        
+        <div className="contentContainer">
+          <div className="menuCardContainer">
+            <div className="tabsList">
+              <ul>
+                <li>All</li>
+                <li>Pizza</li>
+                <li>Burger</li>
+                <li>Pasta</li>
+                <li>Fries</li>
+              </ul>
+            </div>
+            <div className="tabContent">
+              <div className="allCards">
+                <div className="card">
+                  <div className="imgBox">
+                    <img src={f1} alt="" />
+                  </div>
+                  <div className="contentBox">
+                    <h4>Delicious Pizza</h4>
+                    <p>
+                      Veniam debitis quaerat officiis quasi cupiditate quo,
+                      quisquam velit, magnam voluptatem repellendus sed eaque
+                    </p>
+                    <div className="priceBox">
+                      <p>$20</p>
+                      <FontAwesomeIcon icon={faCartShopping} />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="viewMore">
+            <button>View More</button>
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
