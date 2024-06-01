c0c7e39e8d5086644217d2cd1642c167d71ce5c2
sachingour <gour.sachin.sg@gmail.com>
2024-06-01 15:11:14 +0530

    login page frontend

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,19 +1,40 @@
 import React from "react";
-import login_banner from "../assets/images/login_banner.webp";
+import login_bg from "../assets/images/login_bg.jpg";
+import {Link} from 'react-router-dom'
 
 const Login = () => {
   return (
     <section className="loginSection">
       <div className="wrapper">
         <div className="imgContainer">
-          <img src={login_banner} alt="" />
+          <img src={login_bg} alt="" />
         </div>
         <div className="formContainer">
+          <h2>
+            Welcome to <span>HotCorner !!</span>
+          </h2>
           <form action="">
-
-            
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              required
+            />
+            <input
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              required
+            />
+            <input type="submit" value="Login" />
           </form>
+          <div className="signUpNote">
+          <p>Alreay Have An Account? <Link>Sign Up</Link></p>
         </div>
+        </div>
+        
       </div>
     </section>
   );
