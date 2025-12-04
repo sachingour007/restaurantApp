ab92b281b73aac96280562267600eb4c95fa29b3
sachingour <gour.sachin.sg@gmail.com>
2025-12-04 23:00:47 +0530

    cors added

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 const { errorMiddleware } = require("./middleware/errorMiddleware");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 //Middleware
+app.use(
+	cors({
+		origin: ["http://localhost:5173", ""],
+		credentials: true,
+	})
+);
 app.use(express.json({ limit: "16kb" })); //Json Handle with Body
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //Json Handle with URL
 app.use(express.static("public")); //use for static items
