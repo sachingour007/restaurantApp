c5353026665dca20298eab56cd00642e012f3dc1
sachingour <gour.sachin.sg@gmail.com>
2025-12-12 23:50:09 +0530

    Booking details component

--- client/src/components/UserComponents/BookingShow.jsx
+++ client/src/components/UserComponents/BookingShow.jsx
@@ -4,19 +4,20 @@ const BookingShow = () => {
   return (
     <section className="bookingDetails">
       <div className="secWrapper">
-        <div className="">
-          <div>
+        <h2>Booking Details</h2>
+        <div className="mainContainer">
+          <div className="headings">
             <span>date</span>
             <span>Name</span>
             <span>Persons</span>
             <span>Slot</span>
             <span>Status</span>
           </div>
-          <div>
+          <div className="dataRow">
             <span>22 Dec 2025</span>
             <span>Sachin</span>
             <span>4</span>
-            <span>7 PM – 9 PM</span>
+            <span>7 PM - 9 PM</span>
             <span>Pending</span>
           </div>
         </div>
