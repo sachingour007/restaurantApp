ec24e13cc2a413655a404857aa28c9d624314938
sachingour <gour.sachin.sg@gmail.com>
2025-12-30 11:33:10 +0530

    code change in index

--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,21 @@ const dotenv = require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./db/inddex");
 
-connectDB()
-	.then(() => {
-		app.listen(process.env.PORT || 8080, () => {
-			console.log(`http://localhost:${process.env.PORT}`);
-		});
-	})
-	.catch((err) => {
-		console.log("Server Not Wokring", err);
-	});
+// connectDB()
+// 	.then(() => {
+// 		app.listen(process.env.PORT || 8080, () => {
+// 			console.log(`http://localhost:${process.env.PORT}`);
+// 		});
+// 	})
+// 	.catch((err) => {
+// 		console.log("Server Not Wokring", err);
+// 	});
+
+(async () => {
+	try {
+		await connectDB();
+		console.log("✅ Database connected");
+	} catch (error) {
+		console.error("❌ DB connection failed", error);
+	}
+})();
